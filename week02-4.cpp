46c8026dda9d536b1ce8a1869bfbc62c28811909
yuting8426 <112918648+yuting8426@users.noreply.github.com>
2022-09-13 12:07:39 +0800

    week02

---
+++ week02-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("請用鍵盤輸入2個整數:");
+    scanf("%d", &a );
+    scanf("%d", &b );
+    printf("你讀入了 %d %d \n", a, b );
+    printf("%d", a+b );
+}
+
