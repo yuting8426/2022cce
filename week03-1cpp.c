3398441aaba08ebc6aecdcd51d83b1c8d1d2f575
yuting8426 <112918648+yuting8426@users.noreply.github.com>
2022-09-20 12:00:20 +0800

    week03

---
+++ week03-1cpp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf ("請輸入2個數字: ");
+    scanf("%d%d", &a ,&b );
+
+    printf (" %d + %d 是 %d\n", a, b, a+b);
+    printf (" %d - %d 是 %d\n", a, b, a-b);
+    printf (" %d * %d 是 %d\n", a, b, a*b);
+    printf (" %d / %d 是 %d\n", a, b, a/b);
+    printf (" %d 除法 %d 的餘數是 %d\n", a, b, a%b);
+}
+
