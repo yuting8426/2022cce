cade1bc21d52bb022a2fb7f7a77b56807fedc40a
unknown <a0975315099@gmail.com>
2022-11-22 12:02:47 +0800

    week12

---
+++ week12/week12-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int a[10]={1,5,4,2,9,7,8,3,6,10};
+int main()
+{
+    for(int i=0;i<10;i++){
+        for(int j=i+1;j<10;j++){
+            if(a[i]>a[j]){
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<10;i++)printf("%d ",a[i]);
+}
