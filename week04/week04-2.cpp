e01d1131f7de7eaaafcac7847d323eeb9cc5d07e
yuting8426 <a0975315099@gmail.com>
2023-03-07 12:17:40 +0800

    week04

---
+++ week04/week04-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char line[50];
+	scanf("%s",line);
+
+	int N=strlen(line);
+	int yes=0;
+	for(int i=0;i<N;i++){
+        if(line[i]==line[N-1-i])yes++;
+	}
+	if(yes==N)printf("场常yesΜ霍琌癹ゅ\n");
+	else printf("ぃ琌场常yes,碞ぃ琌癹ゅ\n");
+
+}
